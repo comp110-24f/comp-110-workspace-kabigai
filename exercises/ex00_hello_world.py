3180725f25a161c3177c14169e9712775e458381
Abbey Kollu <kabigai@ad.unc.edu>
2024-08-27 00:29:47 -0400

    Finished Exercise 00!

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,8 @@
+"""My first exervise in COMP110!"""
+
+__author__ = "730476269"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function  definition."""
+    return "Hello, " + name + "!"
---
+++ lessons.functions/lesson 3.py.py
@@ -0,0 +1,11 @@
+# print(randint(3, 17))
+
+
+# function definition
+def sum(num1: int, num2: int) -> int:
+    """return the sum of num1 and num2"""
+    return num1 + num2
+
+
+# function call
+print(sum(num1=2, num2=13))  # <- arguments
